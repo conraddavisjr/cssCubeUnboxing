4ebaebeeeb2efcabeb48759d4d7565aaa0db82d5
Conrad Davis Jr <conraddavis@google.com>
2017-09-12 15:33:56 -0700

    added scrollmagic and gsap animation to the story

--- src/cube-animation/index.js
+++ src/cube-animation/index.js
@@ -1,31 +1,27 @@
-import Component from 'play-web-core/es6/component'
+// import Component from 'play-web-core/es6/component'
 
-class AnimationTest extends Component {
+// init controller
+// var controller = new ScrollMagic.Controller();
+// var openBox = new TweenMax();
+// console.log(openBox);
 
-  constructor(el) {
-    super(el)
-    this.init()
-  }
+// init ScrollMagic controller
+var controller = new ScrollMagic.Controller();
 
-  _elements() {
-    this.circle = this.el.querySelector('.circle')
-  }
+// dom elements
+var topFace = document.querySelector('.top');
 
-  _events() {
-    this._animateCircle()
-  }
+// build tween (openLid)
+var openLid = new TimelineMax();
+openLid.to(topFace, 1, { transform:"rotateX(120deg) translateZ(100px) translateY(-175px)" });
 
-  _animateCircle() {
-    new TimelineMax({
-      repeat: -1
-    }).to(this.circle, 3, {
-      rotationY: '360_ccw',
-      ease: Linear.easeNone
-    })
-  }
 
-}
+// TweenMax.set(topFace, { transform: "rotateX(90deg) translateZ(0px) translateY(-200px)" });
 
-const el = document.getElementById('animation-test')
 
-export default new AnimationTest(el)
+var scene1 = new ScrollMagic.Scene({ triggerElement: "#trigger1" })
+  // .setTween(openBox.to('.top', 1, { transform:"rotateX(120deg) translateZ(100px)" })) // trigger a TweenMax.to tween
+  .setTween(openLid) // trigger a TweenMax.to tween
+  // transform:"rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)"
+  .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+  .addTo(controller);
